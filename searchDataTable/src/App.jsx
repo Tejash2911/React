626f8ad6771efbfe1688858050a4ae0640678440
Tejash2911 <tcpatel2911@gmail.com>
2023-09-24 18:09:26 +0530

    Render Search And Table Component In App

--- searchDataTable/src/App.jsx
+++ searchDataTable/src/App.jsx
@@ -1,11 +1,30 @@
-import "./App.css";
+import React, { useState } from "react";
+import Table from "./Table";
+import Search from "./Search";
 
-function App() {
+const App = () => {
+  const data = [
+    { id: 1, name: "Alice", age: 28 },
+    { id: 2, name: "Bob", age: 32 },
+    { id: 3, name: "Charlie", age: 22 },
+    // Add more data here...
+  ];
+
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearch = (term) => {
+    // Perform the filtering logic here based on the search term
+    const filtered = data.filter((item) => item.name.toLowerCase().includes(term.toLowerCase()));
+    setFilteredData(filtered);
+  };
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div>
+      <h1>Table with Search</h1>
+      <Search onSearch={handleSearch} />
+      <Table data={filteredData} />
+    </div>
   );
-}
+};
 
 export default App;
