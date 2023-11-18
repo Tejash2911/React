fe45a33bc1f9fb387bc7c533e476f67b3e5ba22b
Tejash2911 <tcpatel2911@gmail.com>
2023-11-18 19:53:19 +0530

    User Management Using React-Redux

--- userManagement/src/components/User/UserCom.jsx
+++ userManagement/src/components/User/UserCom.jsx
@@ -1,4 +1,4 @@
-import { Button } from "reactstrap";
+import { Button, FormGroup, Input } from "reactstrap";
 import UserForm from "./UserForm";
 import UserList from "./UserList";
 import { useState } from "react";
@@ -13,10 +13,18 @@ const UserCom = () => {
   return (
     <>
       <div className="m-3">
-        <div className="border-bottom d-flex justify-content-start">
+        <div className="border-bottom d-flex justify-content-between">
           <Button className="mb-3" color="primary" size="sm" onClick={() => toggle()}>
             Add User
           </Button>
+          <FormGroup>
+            <Input type="select" id="filterSelect" name="filterSelect">
+              <option value={-1}>Filter</option>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+              <option value="editor">Editor</option>
+            </Input>
+          </FormGroup>
         </div>
         <UserForm modal={modal} toggle={toggle} index={index} setIndex={setIndex} userData={userData} />
         <UserList setIndex={setIndex} toggle={toggle} setUserData={setUserData} />
