5847e239a31e3b0bc85f8b870d0774b2de448e4f
Tejash2911 <tcpatel2911@gmail.com>
2023-12-20 20:41:09 +0530

    E-Commerce Project

--- TejashCreation/Frontend/src/utils/DummyData.js
+++ TejashCreation/Frontend/src/utils/DummyData.js
@@ -80,7 +80,7 @@ export const Categoris = [
         id: 5,
         cat: "home",
         img: "https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Home & Kitchen",
+        title: "Home",
     }
 
 ]
