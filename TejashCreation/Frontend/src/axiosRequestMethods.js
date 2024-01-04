b114798fb45bf317ae1d7c0a53dd36581b38eaf2
Tejash2911 <tcpatel2911@gmail.com>
2024-01-04 23:28:25 +0530

    E-Commerce Project

--- TejashCreation/Frontend/src/axiosRequestMethods.js
+++ TejashCreation/Frontend/src/axiosRequestMethods.js
@@ -6,9 +6,14 @@ const BASE_URL = "http://localhost:4000";
 //in above line i have used optionl chaining and it makes code hard to read so i made a simpler function
 
 function getAccessToken() {
-    const storage = JSON.parse(localStorage.getItem("persist:root"))
-    const currentUser = storage ? JSON.parse(storage.currentUser) : null;
-    return currentUser ? currentUser?.accessToken : null
+    try {
+        const storage = JSON.parse(localStorage.getItem("persist:root"))
+        const currentUser = storage ? JSON.parse(storage.currentUser) : null;
+        return currentUser ? currentUser?.accessToken : null
+    } catch (error) {
+        console.error("Error retrieving access token:", error);
+        return null;
+    }
 }
 
 export const publicRequest = axios.create({
@@ -21,9 +26,13 @@ export const userRequest = axios.create({
 })
 
 userRequest.interceptors.request.use((config) => {
-    const newToken = getAccessToken();
-    if (newToken) {
-        config.headers.token = `Bearer ${newToken}`
+    try {
+        const newToken = getAccessToken();
+        if (newToken) {
+            config.headers.token = `Bearer ${newToken}`;
+        }
+    } catch (error) {
+        console.error("Error updating request with new token:", error);
     }
     return config;
 })
