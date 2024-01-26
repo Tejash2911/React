07dd2af1a8ac18eec42d46104055258e7389a30d
Tejash2911 <tcpatel2911@gmail.com>
2024-01-26 19:28:10 +0530

    Re-Structure Step Component

--- catering_system/src/components/Steps/Steps.jsx
+++ catering_system/src/components/Steps/Steps.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import "./Steps.css";
 
-const Steps = ({ stepData }) => {
+const Steps = ({ data }) => {
   return (
-    <>
-      <div className="steps" id="steps">
-        {stepData.map((curItem) => {
-          return (
-            <div className="box" key={curItem.id}>
-              <img src={curItem.image} alt={curItem.name} />
-              <h3>{curItem.name}</h3>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div className="box">
+      <img src={data.image} alt={data.name} />
+      <h3>{data.name}</h3>
+    </div>
   );
 };
 
