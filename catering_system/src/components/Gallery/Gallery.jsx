d823ec6487381e0d8e298e0e419b4132c07c2681
Tejash2911 <tcpatel2911@gmail.com>
2024-01-26 19:16:05 +0530

    Re-Structure Gallery Component

--- catering_system/src/components/Gallery/Gallery.jsx
+++ catering_system/src/components/Gallery/Gallery.jsx
@@ -6,7 +6,20 @@ import menu from "./GalleryItem.js";
 const Gallery = () => {
   const [menuData, setMenuData] = useState(menu);
 
-  return <GalleryCard menuData={menuData} />;
+  return (
+    <>
+      <section className="gallery" id="gallery">
+        <h1 className="heading">
+          our food <span>gallery</span>{" "}
+        </h1>
+        <div className="box-container">
+          {menuData.map((curElem) => {
+            return <GalleryCard key={curElem.id} menuData={curElem} />;
+          })}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Gallery;
