089442c7f06739595f28935877fc9844b1e30309
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2024-03-22 22:46:02 +0530

    Delete TejashCreation directory

--- TejashCreation/api/utils/pass.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const cryptoJS = require("crypto-js")
-
-const decreptPass = (pass) => cryptoJS.AES.decrypt(pass, process.env.CRYPTOJS_SECRET_KEY).toString(cryptoJS.enc.Utf8)
-
-const encreptPass = (pass) => cryptoJS.AES.encrypt(pass, process.env.CRYPTOJS_SECRET_KEY).toString()
-
-
-module.exports = { decreptPass, encreptPass }
